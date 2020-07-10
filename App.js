00bd40b3ef410c72897df49e76dff95ad67b01e8
Matheus <matheusgomes192@hotmail.com>
2020-07-10 07:08:50 -0300

    começando projeto

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/navite';
+
+const Container = styled.View``;
+const Texto = styled.Text``;
+
+function App() {
+  return(
+    <Container>
+      <Texto> Olá Mundo</Texto>
+    </Container>
+  );
+}
+export default App;
